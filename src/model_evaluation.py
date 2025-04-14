65ef444366e157539d7069c951c3184255c9f1d5
poojav5475 <vpooja8693@gmail.com>
2025-04-15 02:47:29 +0530

    experiment tracking using dvcline done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -30,7 +30,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-'''
+
 def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
     try:
@@ -47,7 +47,7 @@ def load_params(params_path: str) -> dict:
     except Exception as e:
         logger.error('Unexpected error: %s', e)
         raise
-'''
+
 
 def load_model(file_path: str):
     """Load the trained model from a file."""
@@ -114,7 +114,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
-        #params = load_params(params_path='params.yaml')
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -123,14 +123,14 @@ def main():
 
         metrics = evaluate_model(clf, X_test, y_test)
 
-        '''# Experiment tracking using dvclive
+        # Experiment tracking using dvclive
         with Live(save_dvc_exp=True) as live:
             live.log_metric('accuracy', accuracy_score(y_test, y_test))
             live.log_metric('precision', precision_score(y_test, y_test))
             live.log_metric('recall', recall_score(y_test, y_test))
 
             live.log_params(params)
-        '''
+        
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
